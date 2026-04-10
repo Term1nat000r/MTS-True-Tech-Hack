0be6b119955ecc731a0b775b6cba892189420020
gusslik <grigory.bocharsky@gmail.com>
2026-04-10 20:03:56 +0300

    Добавлены временные заглушки агентов

---
+++ src/agents/stubs.py
@@ -0,0 +1,23 @@
+# Заглушки агентов
+from agents.base import AgentInput, AgentOutput, BaseAgent
+
+# Заглушка агента-генератора
+class GeneratorAgent(BaseAgent):
+    def run(self, input: AgentInput) -> AgentOutput:
+        return AgentOutput(
+            result=f"[ЗАГЛУШКА] Здесь будет сгенерированный агентом код"
+        )
+
+# Заглушка агента-валидатора кода
+class ValidationAgent(BaseAgent):
+    def run(self, input: AgentInput) -> AgentOutput:
+        return AgentOutput(
+            result=f"[ЗАГЛУШКА] Здесь будет результат от агента-валидатора"
+        )
+
+# Заглушка агента-обработчика промптов
+class ProcessAgent(BaseAgent):
+    def run(self, input: AgentInput) -> AgentOutput:
+        return AgentOutput(
+            result=f"[ЗАГЛУШКА] Здесь будет результат от агента-обработчика"
+        )
