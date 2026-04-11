cf800512100d19b0fadb1c92a2338dd69a36cde2
gusslik <grigory.bocharsky@gmail.com>
2026-04-11 05:04:44 +0300

    Добавлены тесты заглушек агента-валидатора и агента-адаптера

--- src/api/main.py
+++ src/api/main.py
@@ -2,7 +2,7 @@
 
 from fastapi import FastAPI
 
-from src.agents.stubs import GeneratorAgent
+from src.agents.stubs import GeneratorAgent, ValidatorAgent, AdapterAgent
 from src.agents.contracts.base import StubAgentInput
 
 app = FastAPI()
@@ -14,4 +14,14 @@ def read_root():
 @app.get("/test_gen")
 async def test_agent_generator():
     result = await GeneratorAgent().run(StubAgentInput(data="Hello World"))
-    return {"result": result}
+    return result
+
+@app.get("/test_val")
+async def test_agent_validator():
+    result = await ValidatorAgent().run(StubAgentInput(data="Hello World"))
+    return result
+
+@app.get("/test_ad")
+async def test_agent_adapter():
+    result = await AdapterAgent().run(StubAgentInput(data="Hello World"))
+    return result
