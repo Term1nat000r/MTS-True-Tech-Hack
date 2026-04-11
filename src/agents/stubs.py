34a41e1b7c6688387d333809cb5f085d5c0a61e1
gusslik <grigory.bocharsky@gmail.com>
2026-04-11 15:16:21 +0300

    Начало работы с контрактами агентов в заглушках

--- src/agents/stubs.py
+++ src/agents/stubs.py
@@ -1,15 +1,18 @@
 # Заглушки агентов
 
+from agents.contracts.generator_contract import GeneratorOutput, Header, Payload, Metadata
 from src.agents.contracts.base import BaseAgent, StubAgentOutput, StubAgentInput
-# from agents.contracts.generator_contract import GeneratorOutput
 # from agents.contracts.validator_contract import ValidatorOutput
 # from agents.contracts.adapter_contract import AdapterOutput
 
 # Заглушка агента-генератора
 class GeneratorAgent(BaseAgent):
-    async def run(self, inp: StubAgentInput) -> StubAgentOutput:
-        return StubAgentOutput(
-            result=f"[ЗАГЛУШКА] Здесь будет результат агента-генератора"
+    async def run(self, inp: StubAgentInput) -> GeneratorOutput:
+        return GeneratorOutput(
+            header=Header(source_agent=""),
+            payload= Payload(),
+            metadata= Metadata(),
+            error= None
         )
 
 # Заглушка агента-валидатора кода
@@ -20,8 +23,8 @@ class ValidatorAgent(BaseAgent):
         )
 
 # Заглушка агента-обработчика промптов
-class AdapterAgent(BaseAgent):
+class ClarifierAgent(BaseAgent):
     async def run(self, inp: StubAgentInput) -> StubAgentOutput:
         return StubAgentOutput(
-            result=f"[ЗАГЛУШКА] Здесь будет результат агента-адаптера"
+            result=f"[ЗАГЛУШКА] Здесь будет результат агента-уточнителя"
         )
