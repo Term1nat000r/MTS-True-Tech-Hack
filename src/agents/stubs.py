99bfc6a99f452eee0701ba9f1070f23f0c1a0e2d
gusslik <grigory.bocharsky@gmail.com>
2026-04-11 15:41:12 +0300

    Сделал генерацию вывода заглушки агентом-генератором

--- src/agents/stubs.py
+++ src/agents/stubs.py
@@ -1,6 +1,6 @@
 # Заглушки агентов
 
-from agents.contracts.generator_contract import GeneratorOutput, Header, Payload, Metadata
+from src.agents.contracts.generator_contract import GeneratorOutput, Header, Payload, Metadata
 from src.agents.contracts.base import BaseAgent, StubAgentOutput, StubAgentInput
 # from agents.contracts.validator_contract import ValidatorOutput
 # from agents.contracts.adapter_contract import AdapterOutput
@@ -8,12 +8,7 @@ from src.agents.contracts.base import BaseAgent, StubAgentOutput, StubAgentInput
 # Заглушка агента-генератора
 class GeneratorAgent(BaseAgent):
     async def run(self, inp: StubAgentInput) -> GeneratorOutput:
-        return GeneratorOutput(
-            header=Header(source_agent=""),
-            payload= Payload(),
-            metadata= Metadata(),
-            error= None
-        )
+        return GeneratorOutput.stub()
 
 # Заглушка агента-валидатора кода
 class ValidatorAgent(BaseAgent):
