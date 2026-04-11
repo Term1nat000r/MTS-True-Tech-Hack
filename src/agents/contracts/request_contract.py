b5575f1601e633bcaaad9dd80c385c4a78923100
gusslik <grigory.bocharsky@gmail.com>
2026-04-11 21:43:14 +0300

    Добавлен контракт запроса

---
+++ src/agents/contracts/request_contract.py
@@ -0,0 +1,34 @@
+# Контракт вывода от агента-уточнителя
+
+from pydantic import BaseModel
+
+class Header(BaseModel):
+    source_agent: str = "user"
+    request_id: str
+    status: str = "new"
+
+class Settings(BaseModel):
+    target_language: str = "ru"
+    mode: str = "code_generation"
+    stream: bool = False
+
+class Payload(BaseModel):
+    raw_prompt: str
+    settings: Settings
+
+class Metadata(BaseModel):
+    timestamp: int
+
+class Request(BaseModel):
+    header: Header
+    payload: Payload
+    metadata: Metadata
+
+    @staticmethod
+    def stub() -> "Request":
+        header = Header(request_id="uuid")
+        settings = Settings()
+        payload = Payload(raw_prompt="Hello World", settings=settings)
+        metadata = Metadata(timestamp=1)
+
+        return Request(header=header, payload=payload, metadata=metadata)
