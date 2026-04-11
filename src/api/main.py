b75d0fa293bcfc788c2947f644e14d9b8ca60ba4
gusslik <grigory.bocharsky@gmail.com>
2026-04-11 04:59:14 +0300

    Добавлен тест работы заглушки агента-генератора в main.py

--- src/api/main.py
+++ src/api/main.py
@@ -10,3 +10,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.get("/test_gen")
+async def test_agent_generator():
+    result = await GeneratorAgent().run(StubAgentInput(data="Hello World"))
+    return {"result": result}
