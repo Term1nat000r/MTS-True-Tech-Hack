bd98e10aa10c75165fd523ad906688d81679547f
gusslik <grigory.bocharsky@gmail.com>
2026-04-11 21:36:35 +0300

    Добавлена функция с методом POST для проверки работы оркестратора

--- src/api/main.py
+++ src/api/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from src.agents.orchestrator import Orchestrator
 from src.agents.stubs import GeneratorAgent, ValidatorAgent, ClarifierAgent
 from src.agents.contracts.base import StubAgentInput
+from src.agents.contracts.input_contract import AgentInput
 
 app = FastAPI()
 
@@ -27,7 +28,14 @@ async def test_agent_clarifier():
     result = await ClarifierAgent().run(StubAgentInput(data="Hello World"))
     return result
 
-@app.get("/test_orc")
-async def test_orchestrator():
-    result = await Orchestrator().run(StubAgentInput(data="Hello World"))
+@app.post("/generate")
+async def generate_code():
+    generator_agent = GeneratorAgent()
+    validator_agent = ValidatorAgent()
+    clarifier_agent = ClarifierAgent()
+
+    orchestrator = Orchestrator(generator_agent=generator_agent, validator_agent=validator_agent, clarifier_agent=clarifier_agent)
+
+    result = await orchestrator.run()
+
     return result
