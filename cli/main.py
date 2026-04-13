28daf28853432d384f72680774ddddd05758a345
gusslik <grigory.bocharsky@gmail.com>
2026-04-14 01:25:30 +0300

    Сделаны все главные функции клиента

--- cli/main.py
+++ cli/main.py
@@ -1,4 +1,6 @@
 import asyncio
+from time import time
+
 import httpx
 
 
@@ -14,12 +16,13 @@ def show_main_menu():
     print("──────────────────────────────────────")
 
 def validate_choice(choice: int) -> bool:
-    return choice >= 1 and choice <= 4
+    return 1 <= choice <= 4
 
-def get_choice() -> int:
+
+def get_choice(message: str) -> int:
     while True:
         try:
-            choice = int(input("Выберите пункт меню\n"))
+            choice = int(input(message))
         except:
             print("Пожалуйста, введите число")
             continue
@@ -29,41 +32,95 @@ def exit_cli():
     print("Выход из Lua Code Generator...")
     exit()
 
-async def process_main_menu_choice(choice: int):
-    async with httpx.AsyncClient(base_url=BASE_URL, timeout=180.0) as client:
-        if(choice == 1):
-            await create_chat(client)
-        elif(choice == 2):
-            pass
-        elif(choice == 3):
-            pass
-        elif(choice == 4):
-            exit_cli()
+async def process_main_menu_choice(client: httpx.AsyncClient, choice: int):
+    if choice == 1:
+        await create_chat(client)
+    elif choice == 2:
+        await choose_chat(client)
+    elif choice == 3:
+        await delete_chat(client)
+
+    elif choice == 4:
+        exit_cli()
+
 
 async def main():
-    client = httpx.AsyncClient()
-
     print("\n╔══════════════════════════════════════╗")
     print("║         Lua Code Generator           ║")
     print("╚══════════════════════════════════════╝")
 
-    while True:
-        if not in_chat:
-            show_main_menu()
-            choice = get_choice()
+    async with httpx.AsyncClient(base_url=BASE_URL, timeout=180.0) as client:
+        global in_chat
+        while True:
+            if not in_chat:
+                show_main_menu()
+                choice = get_choice("Выберите пункт меню: ")
 
-            while not validate_choice(choice):
-                print("Неправильный ввод попробуйте еще раз...")
-                choice = get_choice()
+                while not validate_choice(choice):
+                    print("Неправильный ввод, попробуйте ещё раз...")
+                    choice = get_choice("Выберите пункт меню: ")
 
-            await process_main_menu_choice(choice)
+                await process_main_menu_choice(client, choice)
 
-        else:
-            pass
-            # Получить историю переписки и вывести её
-            # Запросить ввод у пользователя
-            # Вывести ответ от модели
-            # Повторять пока пользователь не прекратит диалог
+            else:
+                user_input = input("Введите запрос (введите \"выйти\" чтобы закрыть чат): ")
+
+                if user_input.lower() == "выйти":
+                    in_chat = False
+                else:
+                    await send_user_input(client, user_input)
+
+async def print_history(client: httpx.AsyncClient):
+    response = await client.get("/get_current_session")
+    session_id = response.json()["session_id"]
+
+    response = await client.get(f"/get_history/{session_id}")
+    history = response.json()["history"]
+
+    for h in history:
+        if h["role"] == "user":
+            print("Вы:", h["content"])
+        elif h["role"] == "assistant":
+            print("Модель:", h["content"])
+
+async def get_current_session_id(client: httpx.AsyncClient):
+    session_id = await client.get("/get_current_session")
+    return session_id.json()["session_id"]
+
+async def send_user_input(client: httpx.AsyncClient, user_input: str):
+    session_id = await get_current_session_id(client)
+    response = await client.post("/test_generate",
+                                 json={
+                                     "header": {
+                                         "source_agent": "user",
+                                         "session_id": session_id or "",
+                                         "status": "new"
+                                     },
+                                     "payload": {
+                                         "raw_prompt": user_input,
+                                         "settings": {
+                                             "target_language": "ru",
+                                             "mode": "code_generation",
+                                             "stream": False
+                                         },
+                                         "history": []
+                                     },
+                                     "metadata": {
+                                         "timestamp": int(time())
+                                     }
+                                 })
+    result = response.json()
+
+    if response.status_code == 500:
+        print("Ошибка:", result["detail"]["message"])
+        return
+
+    if "message" in result:
+        # Уточнение — модель задаёт вопрос
+        print("Модель:", result["message"])
+    elif "code" in result:
+        # Готовый код
+        print("Модель:", result["code"])
 
 async def create_chat(client: httpx.AsyncClient):
     # Создать новую сессию
@@ -82,4 +139,53 @@ async def change_chat(client: httpx.AsyncClient, session_id: str):
     await client.post(f"/change_session/{session_id}")
 
     in_chat = True
-asyncio.run(main())
+
+
+async def get_chats(client: httpx.AsyncClient):
+    chats = await client.get("/get_sessions")
+    return chats.json()["sessions"]
+
+def show_chats(chats):
+    for (i, chat) in enumerate(chats, start=1):
+        print(f"{i}. {chat}")
+    print("──────────────────────────────────────")
+
+async def choose_chat(client: httpx.AsyncClient):
+    chats = await get_chats(client)
+    if len(chats) == 0:
+        print("Нет созданных чатов")
+        print("──────────────────────────────────────")
+    else:
+        show_chats(chats)
+        choice = get_choice("Выберите номер чата: ")
+
+        while not (1 <= choice <= len(chats)):
+            print("Неправильный ввод, попробуйте ещё раз...")
+            choice = get_choice("Выберите номер чата: ")
+
+        session_id = chats[choice - 1]
+
+        await change_chat(client, session_id)
+
+        await print_history(client)
+
+async def delete_chat(client: httpx.AsyncClient):
+    chats = await get_chats(client)
+    if len(chats) == 0:
+        print("Нет созданных чатов")
+        print("──────────────────────────────────────")
+    else:
+        show_chats(chats)
+        choice = get_choice("Выберите номер чата: ")
+
+        while not (1 <= choice <= len(chats)):
+            print("Неправильный ввод, попробуйте ещё раз...")
+            choice = get_choice("Выберите номер чата: ")
+
+        session_id = chats[choice - 1]
+
+        await client.delete(f"/delete_session/{session_id}")
+        print("Чат успешно удалён")
+
+if __name__ == "__main__":
+    asyncio.run(main())
