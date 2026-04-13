47ec44663c6c2dda1a9b26631a872f6c09f026e4
gusslik <grigory.bocharsky@gmail.com>
2026-04-13 05:19:24 +0300

    Добавлена функция создания нового чата в cli

--- cli/main.py
+++ cli/main.py
@@ -4,10 +4,9 @@ import httpx
 
 BASE_URL = "http://localhost:8000/"
 
+in_chat = False
+
 def show_main_menu():
-    print("\n╔══════════════════════════════════════╗")
-    print("║         Lua Code Generator           ║")
-    print("╚══════════════════════════════════════╝")
     print("  [1] Создать новый чат")
     print("  [2] Выбрать чат")
     print("  [3] Удалить чат")
@@ -30,23 +29,23 @@ def exit_cli():
     print("Выход из Lua Code Generator...")
     exit()
 
-def process_main_menu_choice(choice: int):
-    if(choice == 1):
-        pass
-    elif(choice == 2):
-        pass
-    elif(choice == 3):
-        pass
-    elif(choice == 4):
-        exit_cli()
-
-
+async def process_main_menu_choice(choice: int):
+    async with httpx.AsyncClient(base_url=BASE_URL, timeout=180.0) as client:
+        if(choice == 1):
+            await create_chat(client)
+        elif(choice == 2):
+            pass
+        elif(choice == 3):
+            pass
+        elif(choice == 4):
+            exit_cli()
 
 async def main():
-    session_id = None
     client = httpx.AsyncClient()
 
-    in_chat = False
+    print("\n╔══════════════════════════════════════╗")
+    print("║         Lua Code Generator           ║")
+    print("╚══════════════════════════════════════╝")
 
     while True:
         if not in_chat:
@@ -57,7 +56,7 @@ async def main():
                 print("Неправильный ввод попробуйте еще раз...")
                 choice = get_choice()
 
-            process_main_menu_choice(choice)
+            await process_main_menu_choice(choice)
 
         else:
             pass
@@ -66,4 +65,21 @@ async def main():
             # Вывести ответ от модели
             # Повторять пока пользователь не прекратит диалог
 
+async def create_chat(client: httpx.AsyncClient):
+    # Создать новую сессию
+    response = await client.post("/create_session")
+    result = response.json()
+    session_id = result["session_id"]
+    chat_name = result["chat_name"]
+
+    # Сразу её выбрать как активную
+    await change_chat(client, session_id)
+    print(f"Создан новый чат:", chat_name)
+
+async def change_chat(client: httpx.AsyncClient, session_id: str):
+    global in_chat
+
+    await client.post(f"/change_session/{session_id}")
+
+    in_chat = True
 asyncio.run(main())
